dff831e9f7457d721f09eb87118ad49cde853499
RussellMorgan <russell.lambie.morgan@gmail.com>
2022-05-09 13:56:55 -0700

    set release status dynamically

--- src/App.js
+++ src/App.js
@@ -13,7 +13,9 @@ export const INIT_CAMERA_POSITION = [0, 0, -15];
 export const OCTAGON_DISTANCE = 35;
 export const CLOSE_SIDE_DISTANCE = (OCTAGON_DISTANCE / 2) * Math.sqrt(2);
 
+
 function App() {
+  const day = new Date().getDate();
 
   return (
     <ThemeProvider theme={theme}>
@@ -23,7 +25,7 @@ function App() {
           <Canvas gl={{ alpha: false }} >
             <PerspectiveCamera makeDefault position={INIT_CAMERA_POSITION} target={[0, 0, -OCTAGON_DISTANCE]} rotation={[0, 0, 0]} />
             <Scene>
-              <Card url='./assets/WrittenSignedAndDotted.jpeg' title="Written, Signed, & Dotted" description='Released 5-13-22' link='https://distrokid.com/hyperfollow/seansharp/written-signed--dotted' />
+              <Card url='./assets/WrittenSignedAndDotted.jpeg' title="Written, Signed, & Dotted" description={day >= 13 ? 'Released 5-13-22' : 'Releases 5-13-22'} link='https://distrokid.com/hyperfollow/seansharp/written-signed--dotted' />
               <Card url='./assets/WildWorld.png' title="Wild World" description='Released 3-18-22' link='https://distrokid.com/hyperfollow/seansharp/wild-world' />
               <Card url='./assets/takeItSlow.png' title="Take it Slow" description='Released 1-7-22' link='https://distrokid.com/hyperfollow/seansharp/take-it-slow-4' />
               <Card url='./assets/meBeforeYou.jpg' title="Me Before You" description='Released 10-8-21' link='https://distrokid.com/hyperfollow/seansharp/me-before-you-2' />
